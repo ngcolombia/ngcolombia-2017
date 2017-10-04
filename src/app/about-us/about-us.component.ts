026d92dfa5e8d2ce40128734de0398eca3a81afc
Roger Alvarez <roger.alvarez.c@hotmail.com>
2017-10-04 08:52:17 -0500

    feat: about us chips, responsive improvements

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
 	selector: 'app-about-us',
 	templateUrl: './about-us.component.html',
 	styleUrls: ['./about-us.component.scss']
 })
-export class AboutUsComponent implements OnInit {
+export class AboutUsComponent {
 
-	constructor() { }
+	selectedTab = 0;
+	readonly conferenceTabIndex = 0;
+	readonly workshopsTabIndex = 1;
+	readonly foodTabIndex = 2;
 
-	ngOnInit() {
+	showConferenceTab(): void {
+		this.selectedTab = this.conferenceTabIndex;
 	}
 
+	showWorkshopsTab(): void {
+		this.selectedTab = this.workshopsTabIndex;
+	}
+
+	showFoodTab(): void {
+		this.selectedTab = this.foodTabIndex;
+	}
+
+	get conferenceButtonColor(): string {
+		return this.getTabButtonColor(this.conferenceTabIndex);
+	}
+
+	get workshopsButtonColor(): string {
+		return this.getTabButtonColor(this.workshopsTabIndex);
+	}
+
+	get foodButtonColor(): string {
+		return this.getTabButtonColor(this.foodTabIndex);
+	}
+
+	getTabButtonColor(tabIndex: number): string {
+		return this.selectedTab === tabIndex ? 'primary' : 'accent';
+	}
 }
--- src/app/speakers/speaker.data.ts
+++ src/app/speakers/speaker.data.ts
@@ -4,7 +4,7 @@ export const speakersData = [
 	new Speaker('Aaron Bailey',
 				// tslint:disable-next-line:max-line-length
 				'Pellentesque maximus malesuada lectus, vel lobortis erat efficitur a. Donec ac auctor enim, at bibendum risus. Phasellus hendrerit arcu et elit sollicitudin, vel tempus nibh condimentum. Proin dolor est, faucibus sit amet commodo vel, hendrerit ut nulla. Donec vitae nisl metus. Maecenas eu dolor et ante maximus auctor nec eget elit. Phasellus eget auctor sem, vel suscipit nunc. Aenean vulputate imperdiet urna, et convallis nulla mattis ut. Fusce nisl tellus, sodales vel libero in, viverra luctus elit. Nam blandit, enim vel ultricies elementum, libero nulla mollis nunc, sed facilisis ante mauris quis ex.',
-				'@mraatron', '/assets/img/profiles/speaker1.jpg'),
+				'@mraatron', './assets/img/profiles/speaker1.jpg'),
 	new Speaker('Gladys Bryant',
 				// tslint:disable-next-line:max-line-length
 				'Pellentesque maximus malesuada lectus, vel lobortis erat efficitur a. Donec ac auctor enim, at bibendum risus. Phasellus hendrerit arcu et elit sollicitudin, vel tempus nibh condimentum. Proin dolor est, faucibus sit amet commodo vel, hendrerit ut nulla. Donec vitae nisl metus. Maecenas eu dolor et ante maximus auctor nec eget elit. Phasellus eget auctor sem, vel suscipit nunc. Aenean vulputate imperdiet urna, et convallis nulla mattis ut. Fusce nisl tellus, sodales vel libero in, viverra luctus elit. Nam blandit, enim vel ultricies elementum, libero nulla mollis nunc, sed facilisis ante mauris quis ex.',
@@ -24,13 +24,13 @@ export const speakersData = [
 	new Speaker('Aaron Bailey',
 				// tslint:disable-next-line:max-line-length
 				'Pellentesque maximus malesuada lectus, vel lobortis erat efficitur a. Donec ac auctor enim, at bibendum risus. Phasellus hendrerit arcu et elit sollicitudin, vel tempus nibh condimentum. Proin dolor est, faucibus sit amet commodo vel, hendrerit ut nulla. Donec vitae nisl metus. Maecenas eu dolor et ante maximus auctor nec eget elit. Phasellus eget auctor sem, vel suscipit nunc. Aenean vulputate imperdiet urna, et convallis nulla mattis ut. Fusce nisl tellus, sodales vel libero in, viverra luctus elit. Nam blandit, enim vel ultricies elementum, libero nulla mollis nunc, sed facilisis ante mauris quis ex.',
-				'@mraatron', '/assets/img/profiles/user5.jpg'),
+				'@mraatron', './assets/img/profiles/user5.jpg'),
 	new Speaker('Aaron Bailey',
 				// tslint:disable-next-line:max-line-length
 				'Pellentesque maximus malesuada lectus, vel lobortis erat efficitur a. Donec ac auctor enim, at bibendum risus. Phasellus hendrerit arcu et elit sollicitudin, vel tempus nibh condimentum. Proin dolor est, faucibus sit amet commodo vel, hendrerit ut nulla. Donec vitae nisl metus. Maecenas eu dolor et ante maximus auctor nec eget elit. Phasellus eget auctor sem, vel suscipit nunc. Aenean vulputate imperdiet urna, et convallis nulla mattis ut. Fusce nisl tellus, sodales vel libero in, viverra luctus elit. Nam blandit, enim vel ultricies elementum, libero nulla mollis nunc, sed facilisis ante mauris quis ex.',
-				'@mraatron', '/assets/img/profiles/user6.jpg'),
+				'@mraatron', './assets/img/profiles/user6.jpg'),
 	new Speaker('Aaron Bailey',
 				// tslint:disable-next-line:max-line-length
 				'Pellentesque maximus malesuada lectus, vel lobortis erat efficitur a. Donec ac auctor enim, at bibendum risus. Phasellus hendrerit arcu et elit sollicitudin, vel tempus nibh condimentum. Proin dolor est, faucibus sit amet commodo vel, hendrerit ut nulla. Donec vitae nisl metus. Maecenas eu dolor et ante maximus auctor nec eget elit. Phasellus eget auctor sem, vel suscipit nunc. Aenean vulputate imperdiet urna, et convallis nulla mattis ut. Fusce nisl tellus, sodales vel libero in, viverra luctus elit. Nam blandit, enim vel ultricies elementum, libero nulla mollis nunc, sed facilisis ante mauris quis ex.',
-				'@mraatron', '/assets/img/profiles/speaker8.jpg')
+				'@mraatron', './assets/img/profiles/speaker8.jpg')
 ];
