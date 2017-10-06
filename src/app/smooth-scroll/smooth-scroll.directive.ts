fb65cb5f3a7b1e4ed60f9dcea227cbe8a18bb9b3
Unknown <roger.alvarez.c@hotmail.com>
2017-10-06 08:41:46 -0500

    feat: animations for main menu and back to top button added

--- src/app/smooth-scroll/smooth-scroll.directive.ts
+++ src/app/smooth-scroll/smooth-scroll.directive.ts
@@ -5,58 +5,16 @@ import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 })
 export class SmoothScrollDirective {
 	@Input() navigateToElement: string;
+	currentScrollY: number;
+	targetScrollPosition: number;
+	currentTime: number;
+	time: number;
+	easing = 'easeOutSine';
+	speed = 1500;
 
 	constructor(element: ElementRef) { }
 
-	scrollTo(yPoint: number, duration: number): void {
-		setTimeout(() => {
-			window.scrollTo(0, yPoint);
-		}, duration);
-		return;
-	}
-
-	smoothScroll(targetElementId: string): void {
-		const startY = this.getCurrentYPosition();
-		const stopY = this.getTargetElementYPosition(targetElementId);
-		const distance = stopY > startY ? stopY - startY : startY - stopY;
-
-		if (distance < 100) {
-			window.scrollTo(0, stopY); return;
-		}
-		let speed = Math.round(distance / 100);
-
-		if (speed >= 20) {
-			speed = 20;
-		}
-
-		const step = Math.round(distance / 100);
-		let leapY = stopY > startY ? startY + step : startY - step;
-		let timer = 0;
-		// Up to bottom direction
-		if (stopY > startY) {
-			for (let i = startY; i < stopY; i += step) {
-				this.scrollTo(leapY, timer * speed);
-				leapY += step;
-				if (leapY > stopY) {
-					leapY = stopY;
-					timer++;
-				}
-			}
-			return;
-		}
-
-		// from bottom to top direction
-		for (let i = startY; i > stopY; i -= step) {
-			this.scrollTo(leapY, timer * speed);
-			leapY -= step;
-			if (leapY < stopY) {
-				leapY = stopY; 
-				timer++;
-			}
-		}
-	}
-
-	getCurrentYPosition(): number {
+	private getCurrentYPosition(): number {
 		// Firefox, Chrome, Opera, Safari
 		if (self.pageYOffset) {
 			return self.pageYOffset;
@@ -72,7 +30,7 @@ export class SmoothScrollDirective {
 		return 0;
 	}
 
-	getTargetElementYPosition(targetElementId) {
+	private getTargetElementYPosition(targetElementId) {
 		const elm = document.getElementById(targetElementId);
 		let y = elm.offsetTop;
 		let node = elm;
@@ -82,6 +40,36 @@ export class SmoothScrollDirective {
 			y += node.offsetTop;
 		}
 		return y;
+	}
+
+	// Easing equations from https://github.com/danro/easing-js/blob/master/easing.js
+	private easeOutSine(pos): number {
+		return Math.sin(pos * (Math.PI / 2));
+	}
+
+	tick() {
+		this.currentTime += 1 / 60;
+		console.log(`current time ${this.currentTime}, time ${this.time}`);
+		const p = this.currentTime / this.time;
+		const t = this.easeOutSine(p);
+		console.log(`p:  ${p}, time ${t}`);
+		if (p < 1) {
+			requestAnimationFrame(() => this.tick());
+			console.log(this.currentScrollY + ((this.targetScrollPosition - this.currentScrollY) * t));
+
+			window.scrollTo(0, this.currentScrollY + ((this.targetScrollPosition - this.currentScrollY) * t));
+		} else {
+			console.log('scroll done');
+			window.scrollTo(0, this.targetScrollPosition);
+		}
+	}
+
+	smoothScroll(targetElementId: string): void {
+		this.currentScrollY =  this.getCurrentYPosition();
+		this.targetScrollPosition = this.getTargetElementYPosition(targetElementId);
+		this.currentTime = 0;
+		this.time = Math.max(.1, Math.min(Math.abs(this.currentScrollY - this.targetScrollPosition) / this.speed, .8));
+		this.tick();
 	}
 
 	@HostListener('click', ['$event']) onNavigate($event) {
