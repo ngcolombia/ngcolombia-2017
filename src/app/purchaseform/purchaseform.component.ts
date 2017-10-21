ee70909f27ee962ca27038a3b2eb21c06b32db35
Leonardo Pérez Marulanda <leono286@gmail.com>
2017-10-20 20:40:57 -0500

    PurchaseForm is ready. Still to be done: workflow after submit.

--- src/app/purchaseform/purchaseform.component.ts
+++ src/app/purchaseform/purchaseform.component.ts
@@ -18,44 +18,98 @@ const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"
 export class PurchaseformComponent implements OnInit {
 
   attendantInformationForm: FormGroup;
-
   captchaIsValid = false;
+  purchaseTotal;
 
   attendantInformation = {
-    fullName: '',
+    customerId: '',
+    customerName: '',
     email: '',
-    emailConfirmation: '',
-    idNumber: '',
-    phone: ''
+    confirmationEmail: '',
+    customerPhoneNumber: '',
+    tickets: [],
+    tokenCaptcha: ''
+  }
+  
+  purchaseItemsLabels = {
+    'main-conference': {
+      name: 'Main conference',
+      cost: 25,   
+    },
+    'workshop-1': {
+      name: 'Workshop 1',
+      cost: 20,   
+    },
+    'workshop-2': {
+      name: 'Workshop 2',
+      cost: 20,   
+    }
   }
 
   constructor() { }
 
   ngOnInit(): void {
+    
+    if (localStorage.purchaseItems) {
+      let itemsString = localStorage.purchaseItems
+      
+      if( itemsString.indexOf('mc') != -1 ) {
+        this.attendantInformation.tickets.push({ticketCode: 'main-conference'});
+      }
+      if( itemsString.indexOf('w1') != -1 ) {
+        this.attendantInformation.tickets.push({ticketCode: 'workshop-1'});
+      }
+      if( itemsString.indexOf('w2') != -1 ) {
+        this.attendantInformation.tickets.push({ticketCode: 'workshop-2'});
+      }
+      this.calculatePurchaseTotal();
+    } else {
+      // ToDo: redirect user if no  localStorage.purchaseItems
+    }
+
     this.attendantInformationForm = new FormGroup({
-      'fullName': new FormControl(this.attendantInformation.fullName, [Validators.required]),
+      'fullName': new FormControl(this.attendantInformation.customerName, [Validators.required]),
       'email': new FormControl(this.attendantInformation.email, [
         Validators.required,
         Validators.pattern(EMAIL_REGEX)]),
-      'emailConfirmation': new FormControl(this.attendantInformation.emailConfirmation, [
+      'emailConfirmation': new FormControl(this.attendantInformation.confirmationEmail, [
         Validators.required,
         Validators.pattern(EMAIL_REGEX)]),
-      'idNumber': new FormControl(this.attendantInformation.idNumber, [
+      'idNumber': new FormControl(this.attendantInformation.customerId, [
         Validators.required,
         Validators.pattern(IDNUMBER_REGEX)]),
-      'phone': new FormControl(this.attendantInformation.phone, [
+      'phone': new FormControl(this.attendantInformation.customerPhoneNumber, [
         Validators.required,
         Validators.pattern(PHONE_REGEX)])
     }, matchingEmails('email', 'emailConfirmation'));
   }
 
 
-  resolved(captchaResponse: string) {
-    this.captchaIsValid = captchaResponse !== null ? true : false;
+  calculatePurchaseTotal() {
+    let total = 0;
+    this.attendantInformation.tickets.forEach( (ticket)=>{
+      total += this.purchaseItemsLabels[ticket.ticketCode].cost;
+    });
+    this.purchaseTotal = total;
   }
 
-  onSubmit() {
 
+  removeItem(index) {
+    this.attendantInformation.tickets.splice(index,1);
+    this.calculatePurchaseTotal();
+  }
+
+
+  resolved(captchaResponse: string) {
+    this.captchaIsValid = captchaResponse !== null ? true : false;
+    this.attendantInformation.tokenCaptcha = captchaResponse;
+  }
+
+
+  onSubmit() {
+    if( this.attendantInformationForm && this.captchaIsValid ){
+      //ToDo: submit actions
+    }
   }
 
 }
