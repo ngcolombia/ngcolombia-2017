7200f507d5701edf3f8893c7bc2fbfa82061515d
jdjuan <david.juanherrera@gmail.com>
2017-10-23 17:08:25 -0500

    fix: correct navbar errors

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,16 +1,28 @@
+import { ActivatedRoute, Params } from '@angular/router';
 import { MdSidenav } from '@angular/material';
-import { Component, ViewChild, HostListener } from '@angular/core';
+import { Component, ViewChild, HostListener, OnInit } from '@angular/core';
 
 @Component({
 	selector: 'app-main-page',
 	templateUrl: './main-page.component.html',
 	styleUrls: ['./main-page.component.scss']
 })
-export class MainPageComponent {
+export class MainPageComponent implements OnInit {
 
 	hideBackToTop = true;
+	showNavbar = true;
 
 	@ViewChild(MdSidenav) sidenav: MdSidenav;
+
+	constructor(private route: ActivatedRoute) {
+
+	}
+
+	ngOnInit() {
+		this.route.queryParams.subscribe((params: Params) => {
+			this.showNavbar = params['key'] !== 'i_will_attend_ng-colombia_fuck_yeah';
+		});
+	}
 
 	closeNav(selected: boolean): void {
 		this.sidenav.close();
