4c586254f0ff9ccf5c8e7ec26a6f4cea9b83f7d6
Carlos Angulo <carlosalbertoangulomendoza@gmail.com>
2017-10-22 19:25:24 -0500

    feat: adding Community Partners

--- src/app/community-partners/community-partners.component.ts
+++ src/app/community-partners/community-partners.component.ts
@@ -9,29 +9,15 @@ export class CommunityPartnersComponent implements OnInit {
 
 	partners = [
 		{
-			name: 'partner 1',
-			url: './../../assets/img/google-developers-experts.png'
+			name: 'Google Developer Expert Medellin',
+			imgUrl: '/assets/img/gdg.png',
+			url: 'https://www.meetup.com/GDG-Medellin'
 		},
 		{
-			name: 'partner 2',
-			url: './../../assets/img/google-developers-experts.png'
-		},
-		{
-			name: 'partner 3',
-			url: './../../assets/img/google-developers-experts.png'
-		},
-		{
-			name: 'partner 4',
-			url: './../../assets/img/google-developers-experts.png'
-		},
-		{
-			name: 'partner 5',
-			url: './../../assets/img/google-developers-experts.png'
-		},
-		{
-			name: 'partner 6',
-			url: './../../assets/img/google-developers-experts.png'
-		},
+			name: 'Pioneras Developers',
+			imgUrl: '/assets/img/pioneras.png',
+			url: 'https://www.meetup.com/PionerasDevelopers/'
+		}
 	];
 
 	constructor() { }
