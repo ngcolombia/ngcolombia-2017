35530c51f1fb5709b7e84bf0d557ddf9c784974d
jdjuan <david.juanherrera@gmail.com>
2017-11-13 18:11:34 -0500

    fix: correct heroes typos

--- src/app/heroes/heroes.data.ts
+++ src/app/heroes/heroes.data.ts
@@ -1,137 +1,162 @@
 export const heroesData = [
   {
-    "name": "Juan Herrera",
-    "picture": "./assets/img/heroes/JuanHerrera.png",
-    "phrase": "He learned that real-world impact doesn't come from solo genius, but from collective genius like this one. Thank you all ❤️️."
+    name: 'Juan Herrera',
+    picture: './assets/img/heroes/JuanHerrera.png',
+    phrase:
+      "He learned that real-world impact doesn't come from solo genius, but from collective genius like this one. Thank you all ❤️️."
   },
   {
-    "name": "Sherrry List",
-    "picture": "./assets/img/heroes/Sherrry.png",
-    "phrase": "She gave us advice from the very beginning and saved us a lot of time with her experience."
+    name: 'Sherry List',
+    picture: './assets/img/heroes/Sherrry.png',
+    phrase:
+      'She gave us advice from the very beginning and saved us a lot of time with her experience.'
   },
   {
-    "name": "Maxim Salnikov",
-    "picture": "./assets/img/heroes/Maxim.png",
-    "phrase": "He was the first person we reached for. He listened to us and gave us his extensive advice."
+    name: 'Maxim Salnikov',
+    picture: './assets/img/heroes/Maxim.png',
+    phrase:
+      'He was the first person we reached for. He listened to us and gave us his extensive advice.'
   },
   {
-    "name": "Josué Gutiérrez",
-    "picture": "./assets/img/heroes/Josue.png",
-    "phrase": "Josue was the first person to bring the idea of NgColombia to our heads. He believed in this before anyone else."
+    name: 'Josué Gutiérrez',
+    picture: './assets/img/heroes/Josue.png',
+    phrase:
+      'Josue was the first person to bring the idea of NgColombia to our heads. He believed in this before anyone else.'
   },
   {
-    "name": "Camila Cárdenas",
-    "picture": "./assets/img/heroes/MariaCardenas.png",
-    "phrase": "Her badass skills made the production of NgColombia a reality not possible without her."
+    name: 'Camila Cárdenas',
+    picture: './assets/img/heroes/MariaCardenas.png',
+    phrase:
+      "Her badass skills made the production of NgColombia a reality. It wouldn't have been possible without her."
   },
   {
-    "name": "Juliana Aristizábal",
-    "picture": "./assets/img/heroes/JulianaAristizabal.png",
-    "phrase": "The best designer we could possibly hire, all the arts came from her, from our website to our social media."
+    name: 'Juliana Aristizábal',
+    picture: './assets/img/heroes/JulianaAristizabal.png',
+    phrase:
+      'The best designer we could possibly hire, all the arts came from her, from our website to our social media.'
   },
   {
-    "name": "Ana Sánchez",
-    "picture": "./assets/img/heroes/AnaSanchez.png",
-    "phrase": "Our social media expert, if you ever read a publication, it was her gifted mind making them up."
+    name: 'Ana Sánchez',
+    picture: './assets/img/heroes/AnaSanchez.png',
+    phrase:
+      'Our social media expert, if you ever read a publication, it was her gifted mind making them up.'
   },
   {
-    "name": "Milena Mora",
-    "picture": "./assets/img/heroes/MilenaMora.png",
-    "phrase": "She created NgColombia’s official video, the one we are proud to show everywhere :)."
+    name: 'Milena Mora',
+    picture: './assets/img/heroes/MilenaMora.png',
+    phrase:
+      'She created NgColombia’s official video, the one we are proud to show everywhere :).'
   },
   {
-    "name": "Santiago Cano",
-    "picture": "./assets/img/heroes/SantiagoCano.png",
-    "phrase": "Our Operations VP, or how I’d like to call him, the yes man. Santiago finds success where others can’t. He never doubted NgColombia."
+    name: 'Santiago Cano',
+    picture: './assets/img/heroes/SantiagoCano.png',
+    phrase:
+      "Our Operations VP, or how I’d like to call him, the yes man. Santiago finds success where others can't. He never doubted NgColombia."
   },
   {
-    "name": "Lina Colorado",
-    "picture": "./assets/img/heroes/LinaColorado.png",
-    "phrase": "Our Marketing VP, her extensive knowledge and experience saved us a ton of work. Her strong character kept this conference on track."
+    name: 'Lina Colorado',
+    picture: './assets/img/heroes/LinaColorado.png',
+    phrase:
+      'Our Marketing VP, her extensive knowledge and experience saved us a ton of work. Her strong character kept this conference on track.'
   },
   {
-    "name": "María Llano",
-    "picture": "./assets/img/heroes/MariaLlano.png",
-    "phrase": "Our finance VP. She believed in us from the very first moment, she was there to help us with anything."
+    name: 'María Llano',
+    picture: './assets/img/heroes/MariaLlano.png',
+    phrase:
+      'Our finance VP. She believed in us from the very first moment, she was there to help us with anything.'
   },
   {
-    "name": "Federico Jiménez",
-    "picture": "./assets/img/heroes/FedericoJimenez.png",
-    "phrase": "The man who pulled the trigger. His strong beliefs and management skills made NgColombia a huge success."
+    name: 'Federico Jiménez',
+    picture: './assets/img/heroes/FedericoJimenez.png',
+    phrase:
+      'The man who pulled the trigger. His strong beliefs and management skills made NgColombia a huge success.'
   },
   {
-    "name": "Carlos Angulo",
-    "picture": "./assets/img/heroes/CarlosAngulo.png",
-    "phrase": " His leading skills allowed he and his team to create our website in two weeks. Just two weeks!."
+    name: 'Carlos Angulo',
+    picture: './assets/img/heroes/CarlosAngulo.png',
+    phrase:
+      ' His leading skills allowed he and his team to create our website in two weeks. Just two weeks!.'
   },
   {
-    "name": "Roger Alvarez",
-    "picture": "./assets/img/heroes/RogerAlvarez.png",
-    "phrase": "In just one week he set up an online payment system, helped build the website and fixed tons of bugs. We call him Chuck Norris of development."
+    name: 'Roger Alvarez',
+    picture: './assets/img/heroes/RogerAlvarez.png',
+    phrase:
+      'In just one week he set up an online payment system, helped build the website and fixed tons of bugs. We call him Chuck Norris of development.'
   },
   {
-    "name": "Ricardo Rojas",
-    "picture": "./assets/img/heroes/RicardoRojas.png",
-    "phrase": "He built our registration system. He’s also from Cali 'ois'."
+    name: 'Ricardo Rojas',
+    picture: './assets/img/heroes/RicardoRojas.png',
+    phrase: "He built our registration system. He’s also from Cali 'ois'."
   },
   {
-    "name": "Juan Rendón",
-    "picture": "./assets/img/heroes/JuanRendon.png",
-    "phrase": "If you ever thought: “Woah! What a website!”. It was because Jose did a great job. Also known as Jack Nicholson. Don’t ask why."
+    name: 'Juan Rendón',
+    picture: './assets/img/heroes/JuanRendon.png',
+    phrase:
+      'If you ever thought: “Woah! What a website!”. It was because Jose did a great job. Also known as Jack Nicholson. Don’t ask why.'
   },
   {
-    "name": "Leonardo Pérez",
-    "picture": "./assets/img/heroes/LeonardoPerez.png",
-    "phrase": "His bionic eyes allow him to create pixel perfect interfaces. Our website looks amazing thanks to him. He is color-blind #respect."
+    name: 'Leonardo Pérez',
+    picture: './assets/img/heroes/LeonardoPerez.png',
+    phrase:
+      'His bionic eyes allow him to create pixel perfect interfaces. Our website looks amazing thanks to him. He is color-blind #respect.'
   },
   {
-    "name": "Daniel Correa",
-    "picture": "./assets/img/heroes/DanielCorrea.png",
-    "phrase": "He’s been excited from day one giving us all his support. He got us the JetBrains licenses. He has a pet named 'Brownie' :)."
+    name: 'Daniel Correa',
+    picture: './assets/img/heroes/DanielCorrea.png',
+    phrase:
+      "He’s been excited from day one giving us all his support. He got us the JetBrains licenses. He has a pet named 'Brownie' :)."
   },
   {
-    "name": "Pablo Velásquez",
-    "picture": "./assets/img/heroes/PabloVelasquez.png",
-    "phrase": "He did the toughest and more important job. Hosting our speakers making them feel at home."
+    name: 'Pablo Velásquez',
+    picture: './assets/img/heroes/PabloVelasquez.png',
+    phrase:
+      'He did the toughest and more important job. Hosting our speakers making them feel at home.'
   },
   {
-    "name": "Laura Ciro",
-    "picture": "./assets/img/heroes/LauraCiro.png",
-    "phrase": "The person behind NgGirls. It was her tough work that made this workshop a reality. Also known for disliking smartphones #respect."
+    name: 'Laura Ciro',
+    picture: './assets/img/heroes/LauraCiro.png',
+    phrase:
+      'The person behind NgGirls. It was her tough work that made this workshop a reality. Also known for disliking smartphones #respect.'
   },
   {
-    "name": "Fabián Buitrago",
-    "picture": "./assets/img/heroes/Fabian.png",
-    "phrase": "The person behind our Slack. All the announcements and replies came from him."
+    name: 'Fabián Buitrago',
+    picture: './assets/img/heroes/Fabian.png',
+    phrase:
+      'The person behind our Slack. All the announcements and replies come from him.'
   },
   {
-    "name": "Juan Villa",
-    "picture": "./assets/img/heroes/JuanVilla.png",
-    "phrase": "NgGirls and Workshop #2 tutor. He committed to this conference helping us out constantly."
+    name: 'Juan Villa',
+    picture: './assets/img/heroes/JuanVilla.png',
+    phrase:
+      'NgGirls and Workshop #2 tutor. He committed to this conference helping us out constantly.'
   },
   {
-    "name": "Manuela Castrillón",
-    "picture": "./assets/img/heroes/ManuelaCastrillon.png",
-    "phrase": "Angular lover that helped us as a tutor for the Workshop #1."
+    name: 'Manuela Castrillón',
+    picture: './assets/img/heroes/ManuelaCastrillon.png',
+    phrase: 'Angular lover that helped us as a tutor for the Workshop #1.'
   },
   {
-    "name": "Marian Villa",
-    "picture": "./assets/img/heroes/MarianVilla.png",
-    "phrase": "She helped us designing the art of of the NgGirls workshop. She was very supportive."
+    name: 'Marian Villa',
+    picture: './assets/img/heroes/MarianVilla.png',
+    phrase:
+      'She helped us designing the art of the NgGirls workshop. She was very supportive.'
   },
   {
-    "name": "Carlos Lopez",
-    "picture": "./assets/img/heroes/CarlosLopez.png",
-    "phrase": "He was committed as a tutor for the RxJS workshop. His extensive knowledge was key."
+    name: 'Carlos Lopez',
+    picture: './assets/img/heroes/CarlosLopez.png',
+    phrase:
+      'He was committed as a tutor for the RxJS workshop. His extensive knowledge was key.'
   },
   {
-    "name": "Harlen Giraldo",
-    "picture": "./assets/img/heroes/HarlenGiraldo.png",
-    "phrase": "He was in charge of Angular Medellin twitter. And he did a great job."
+    name: 'Harlen Giraldo',
+    picture: './assets/img/heroes/HarlenGiraldo.png',
+    phrase:
+      'He was in charge of Angular Medellin twitter. And he did a great job.'
   },
   {
-    "name": "Mateo Castaño",
-    "picture": "./assets/img/heroes/MateoCastaño.png",
-    "phrase": "He always supported our community, he helped us by being a tutor in the Workshop #1."
+    name: 'Mateo Castaño',
+    picture: './assets/img/heroes/MateoCastaño.png',
+    phrase:
+      'He always supported our community, he helped us by being a tutor in the Workshop #1.'
   }
 ];
